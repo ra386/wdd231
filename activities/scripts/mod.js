9983031b719659ecadc3e1d6339fafabf12c1bfe
cjking4 <cjgamefowl032@gmail.com>
2025-03-26 22:16:42 +0100

    puy

---
+++ activities/scripts/mod.js
@@ -0,0 +1,49 @@
+
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=metric&appid=342b74d3212f1cbc071c06e243044b2e'
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      // displayResults(data); // uncomment when ready
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', data.weather[0].description);
+  captionDesc.textContent = `${desc}`;
+}
+
+// import aCourse from "./course.mjs";
+
+// aCourse.init();
+
+//   document.querySelector("#enrollStudent").addEventListener("click", function () {
+//     const sectionNum = document.querySelector("#sectionNumber").value;
+//     aCourse.changeEnrollment(sectionNum);
+//   });
+//   document.querySelector("#dropStudent").addEventListener("click", function () {
+//     const sectionNum = document.querySelector("#sectionNumber").value;
+//     aCourse.changeEnrollment(sectionNum, false);
+//   });
+  
+
+ 
